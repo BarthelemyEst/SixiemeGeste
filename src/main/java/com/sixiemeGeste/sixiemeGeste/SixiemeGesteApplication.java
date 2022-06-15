a4c7754c82eda17eba16bbb5cd27cd04f20d78cb
BarthelemyEst <74047314+BarthelemyEst@users.noreply.github.com>
2022-06-15 15:03:09 +0200

    Add files via upload

---
+++ src/main/java/com/sixiemeGeste/sixiemeGeste/SixiemeGesteApplication.java
@@ -0,0 +1,13 @@
+package com.sixiemeGeste.sixiemeGeste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SixiemeGesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SixiemeGesteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sixiemeGeste/sixiemeGeste/SixiemeGesteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sixiemeGeste.sixiemeGeste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SixiemeGesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
